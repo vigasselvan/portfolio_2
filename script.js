b844d415dd1da26f71b5d1c83c65c49a01764e6f
Vigas <77221759+vigasselvan@users.noreply.github.com>
2025-10-15 12:43:25 +0530

    add resposinveness in navigation menu
    add hamburger menu feature when opened in mobile devices

---
+++ script.js
@@ -0,0 +1,16 @@
+function handleClick(){
+    const mobile_Link = document.getElementById("mobile_Links");
+    const hamburger_menu = document.getElementsByClassName("hamburger_menu")
+
+    if(mobile_Link.style.display === "block"){
+        mobile_Link.style.display = "none";
+        hamburger_menu.textContent = "≡";
+        console.log("current hamburger: " + hamburger_menu.textContent);
+        console.log(" changed to none");
+    }else{
+        mobile_Link.style.display = "block";
+        hamburger_menu.textContent = "X";
+        console.log("current hamburger: " + hamburger_menu.textContent);
+        console.log("changed to block");
+    }
+}
